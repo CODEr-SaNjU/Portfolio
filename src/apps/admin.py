a5c2f6abb8edaf2623075cf1aece65e3164520fc
coder-sanju <sanju2help@gmail.com>
2023-04-13 10:12:04 +0530

    about me update and project setup

--- src/apps/admin.py
+++ src/apps/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ContactDetails,Post
 
 # Register your models here.
+
+admin.site.register(ContactDetails)
+admin.site.register(Post)
--- src/apps/models.py
+++ src/apps/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+
+class ContactDetails(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField()
+    mobile_number = models.CharField(max_length=12)
+    message = models.TextField()
+
+
+    def __str__(self) -> str:
+        return f"{self.first_name}  {self.last_name}"
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+
